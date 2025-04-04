fa847e13d019ea6c75845bf27dcd5ec429f5845c
Max <imax.mr@gmail.com>
2025-04-04 09:41:47 -0700

    implemented additional stat methods

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-from src.data.creature import Creature
-
-lil_guy = {
-    "name": "Goblin",
-    "prof_bonus": 2,
-    "type": "humanoid",
-    "stats": [8, 14, 10, 10, 8, 8]
-}
-
-goblin = Creature(lil_guy)
-
-print(goblin.stats.get_summary())
+print("Nothing here yet.")
